c27948808626c1a68189d17b2197dd81eff0aef4
batodalaev <ya.sayanb@gmail.com>
2016-04-28 04:58:12 +0600

    правка

--- chest/ourLibrary.h
+++ chest/ourLibrary.h
@@ -1,12 +1,3 @@
 #pragma once
 
-/*
-UK fast algorithm for counting the line length
-RU быстрый подсчет длины строки
-*/
-_int64 countLeng(char* ST) {
-	char* UM = ST;
-	while (*UM != '\0') ++UM;
-	_int64 n = (UM - ST);
-	return n;
-}
+
