1e208f742ae0383b8d998ad536392a1d3e66fe28
Anatoliy <NoteBook@DESKTOP-M164T6A>
2016-04-28 03:41:44 +0600

    С чистого листа

---
+++ chest/MainForm.cpp
@@ -0,0 +1,11 @@
+#include "MainForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	chest::MainForm form;
+	Application::Run(%form);
+}
