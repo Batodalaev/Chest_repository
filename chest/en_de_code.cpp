a356cef2d49bf3188614ff1ca4dec0e86008a57b
batodalaev <ya.sayanb@gmail.com>
2016-04-28 04:43:26 +0600

    вернул все на место

--- chest/en_de_code.cpp
+++ chest/en_de_code.cpp
@@ -5,6 +5,43 @@
 #include "ourLibrary.h"
 using namespace std;
 
+
+/*
+UK this function is encode text
+RU эта функция шифрует текст
+
+the next texts is just to fun->
+FR cette fonction est encodage du texte
+DE Diese Funktion verschlsselt Text
+*/
+char* encoder(char* textPlain, //Plain text/незашифрованный текст
+	unsigned int textLeng, //text's length/длина текста  
+	char* key, //key/ключ
+	unsigned int keyLeng) { //key's length/длина ключа
+
+	char* textChiper = (char*)malloc(textLeng + 1);
+
+
+	return textChiper;// return pointer/возвращает указатель
+}
+
+
+
+/*
+UK this function is decode text
+RU эта функция дешифрует текст
+
+the next texts is just to fun->
+FR cette fonction est decodage du texte
+DE Diese Funktion entschlsselt Text
+
+*/
+int decoder() {
+
+
+	return 0;//требуются правки/require edit
+}
+
 int main()
 {/*
 	char str[100];
