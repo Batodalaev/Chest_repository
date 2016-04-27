7213597718541b81d34a5c371c6568367531e738
batodalaev <ya.sayanb@gmail.com>
2016-04-28 04:05:03 +0600

    ввел свой код

---
+++ chest/en_de_code.cpp
@@ -0,0 +1,79 @@
+//it is a first file in repozitory(may be)
+//libraries
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+/*
+UK fast algorithm for counting the line length
+RU быстрый подсчет длины строки
+*/
+int countLeng(char* ST) {
+	char* UM = ST;
+	while (*UM != '\0') ++UM;
+	int n = (UM - ST);
+	return n;
+}
+
+/*
+int printString(char* ST) {
+char* UM = ST;
+while (*UM != '\0') {
+++UM;
+cout << *UM;
+};
+return 0;
+}*/
+
+/*
+UK this function is encode text
+RU эта функция шифрует текст
+
+the next texts is just to fun->
+FR cette fonction est encodage du texte
+DE Diese Funktion verschl?sselt Text
+*/
+char* encoder(char* textPlain, //Plain text/незашифрованный текст
+	unsigned int textLeng, //text's length/длина текста  
+	char* key, //key/ключ
+	unsigned int keyLeng) { //key's length/длина ключа
+	char* textChiper = (char*)malloc(textLeng + 1);
+
+	return textChiper;
+}
+
+/*
+UK this function is decode text
+RU эта функция дешифрует текст
+
+the next texts is just to fun->
+FR cette fonction est decodage du texte
+DE Diese Funktion entschl?sselt Text
+
+*/
+int decoder() {
+
+
+	return 0;//требуются правки/require edit
+}
+int main()
+{
+	/*char str[100];
+	char key[100];
+	cin >> str;
+	cin >> key;
+	unsigned int stringLeng = countLeng(str);
+	unsigned int keyLeng = countLeng(key);
+	char* chiper;
+	chiper=encoder(str,stringLeng,key,keyLeng);
+
+	cout << keyLeng << endl
+	<< stringLeng << endl
+	<< endl;
+	printString(str);
+	cout << endl;
+	system("pause");*/
+	return 0;
+}
