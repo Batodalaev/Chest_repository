9981b22e5d5afb35e54f4fa822db0877ba314bf5
Anatoliy <NoteBook@DESKTOP-M164T6A>
2016-04-29 01:28:43 +0600

    GUI 50%

--- chest_win32/main.cpp
+++ chest_win32/main.cpp
@@ -1,9 +1,17 @@
 #include <Windows.h>
-#include <MainDlg.h>
+#include "MainDlg.h"
 
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	
+	MSG msg;
+	MainDlg* Main = new MainDlg(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL);
+	Main->Show(SW_SHOW);
+
+	while (FALSE!=GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 }
 
